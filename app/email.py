c3731a6171c311fd06b0d6e589cb79b36ede41bb
DonAlPatino <valp@inbox.ru>
2024-06-29 11:34:27 +0300

    15 main to blueprint

--- app/email.py
+++ app/email.py
@@ -1,6 +1,6 @@
 from flask_mail import Message
 from app import mail
-from app import app
+from app import current_app
 from threading import Thread
 
 def send_async_email(app, msg):
@@ -11,6 +11,5 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    print(mail.MAIL_SERVER," - ",mail.MAIL_PORT )
-    #mail.send(msg)
-    Thread(target=send_async_email, args=(app, msg)).start()
+    Thread(target=send_async_email,
+           args=(current_app._get_current_object(), msg)).start()
